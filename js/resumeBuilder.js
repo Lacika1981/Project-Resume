454e70c168e8486843f99850f861b9b5ea552df6
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-05-19 22:44:37 +0100

    Data Types Quiz string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var firstName = "Laszlo";
+var age = 36;
+console.log(firstName);
+
+
+var awesomeThoughts = "I am Laszlo Varga and I am AWESOME";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
