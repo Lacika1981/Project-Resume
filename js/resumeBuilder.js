f61b8c96afdbca1bda5232fee4e517d88dd65fa2
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-05-22 14:26:34 +0100

    first object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var firstName = "Laszlo";
+/*var firstName = "Laszlo";
 var age = 36;
 console.log(firstName);
 
@@ -11,4 +11,48 @@ var awesomeThoughts = "I am Laszlo Varga and I am AWESOME";
 console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
-$("#main").append(funThoughts);
+$("#main").append(funThoughts);*/
+
+/*var name = "Laszlo Varga";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);*/
+
+var bio = {
+	"name": "Laszlo Varga",
+	"role": "Web Developer",
+	"contactInfo": {
+		"mobile": "0775812345",
+		"email": "something@extra.com"
+	},
+	"picture": "images/197x148.gif",
+	"message": "Welcome on my BIO page",
+	"skills": ["HTML", "CSS", "bootstrap", "JavaScript"]
+}
+
+var name = HTMLheaderName.replace("%data%", bio["name"]);
+var role = HTMLheaderRole.replace("%data%", bio["role"]);
+
+$("#header").prepend(role);
+$("#header").prepend(name);
+
+var mobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+$("#header").append(mobile);
+
+var email = HTMLemail.replace("%data%", bio["contactInfo"]["email"]);
+$("#header").append(email);
+
+var welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio["message"]);
+$("#header").append(welcomeMsg);
+
+$("#header").append(HTMLskillsStart);
+
+var skills = HTMLskills.replace("%data%", bio["skills"]);
+$("#header").append(skills);
+
+var pic = HTMLbioPic.replace("%data%", bio.picture);
+$("#header").append(pic);
